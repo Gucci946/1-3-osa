1cddad67de5d263479b7ccfc607e9a3ad14231fc
Gucci946 <antonchikgucci@gmail.com>
2026-04-06 12:00:34 +0300

    Add files via upload

---
+++ 1.-osa/Startclass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Channels;
+
+namespace _1._osa
+{
+    internal class Startclass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Tere, kirjuta oma nimi palun.");
+            string nimi = Console.ReadLine();
+            Console.WriteLine($"Tere, {nimi}, kirjuta kaks arvu ja ma korrutan need.");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("{0} ja {1} korrutis on {2}", a, b, a * b);
+        }
+    }
+}
+
